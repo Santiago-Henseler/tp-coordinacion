a0b45b34e6a8a19e8ac8f51fdb68d29cea5cda17
Santy <shenseler@fi.uba.ar>
2026-09-18 18:51:43 -0300

    agrego gitigniore

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -4,14 +4,16 @@ from common import message_protocol
 class MessageHandler:
 
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.userId = str(uuid.uuid4())
+        self.totalMsg = 0
     
     def serialize_data_message(self, message):
         [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount, self.id]) # TODO: podria hacerse de otra manera mas prolijo
+        self.totalMsg += 1
+        return message_protocol.internal.serialize([fruit, amount, self.userId]) # TODO: podria hacerse de otra manera mas prolijo
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([self.id])
+        return message_protocol.internal.serialize([self.userId])
 
     def deserialize_result_message(self, message):
         fields = message_protocol.internal.deserialize(message)
